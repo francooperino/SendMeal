aadd6e6b290cab36d7dd8a543a5cb99804fcf291
brunoperetti <brunoperetti10@gmail.com>
2020-09-10 23:37:18 -0300

    avanzamos y probamos mostrar el valor actual de seekbar

--- app/src/main/java/com/example/lab1appmoviles/MainActivity.java
+++ app/src/main/java/com/example/lab1appmoviles/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.lab1appmoviles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar barra;
+    private TextView valorActual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        barra = (SeekBar) findViewById(R.id.barrita);
+        valorActual = (TextView) findViewById(R.id.textito);
+
+        valorActual.setText(barra.getProgress());
+
     }
+
+
+
 }
